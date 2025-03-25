d489868c6c26825de8d1038b6f3183663c1892b4
Drew Allred <drew.e.allred@gmail.com>
2025-03-25 14:09:28 -0400

    feat: installed animate on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Explore from "./pages/Explore";
-import Author from "./pages/Author";
-import ItemDetails from "./pages/ItemDetails";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Explore from "./pages/Explore"
+import Author from "./pages/Author"
+import ItemDetails from "./pages/ItemDetails"
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-
+AOS.init()
 
 function App() {
   return (
